f2e653760a855467398e04b294402ef220da5d70
Nils Truchaud <truchaud.nils@gmail.com>
2015-06-17 14:46:14 +0200

    Update Entity.swift

--- MaCommercial/MaCommercial/Entity.swift
+++ MaCommercial/MaCommercial/Entity.swift
@@ -3,7 +3,7 @@
 //  MaCommercial
 //
 //  Created by iem on 03/06/2015.
-//  Copyright (c) 2015 Sarah LAFORETS. All rights reserved.
+//  Copyright (c) 2015 Nils Truchaud. All rights reserved.
 //
 
 import Foundation
