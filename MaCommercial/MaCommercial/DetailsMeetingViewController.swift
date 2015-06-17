d6d1943c997f9f56d4cc56b455a88192b6d52d7f
Nils Truchaud <truchaud.nils@gmail.com>
2015-06-17 14:45:45 +0200

    Update DetailsMeetingViewController.swift

--- MaCommercial/MaCommercial/DetailsMeetingViewController.swift
+++ MaCommercial/MaCommercial/DetailsMeetingViewController.swift
@@ -3,7 +3,7 @@
 //  MaCommercial
 //
 //  Created by Sarah LAFORETS on 08/06/2015.
-//  Copyright (c) 2015 Sarah LAFORETS. All rights reserved.
+//  Copyright (c) 2015 Nils Truchaud. All rights reserved.
 //
 
 import UIKit
